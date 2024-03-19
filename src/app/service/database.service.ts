148b1fde04884682c17fd7a951a92d15f4f7666e
Vinayak More <vinayak.more0914@gmail.com>
2024-03-19 07:52:25 +0530

    transaction type changes

--- src/app/service/database.service.ts
+++ src/app/service/database.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, WritableSignal, signal } from '@angular/core';
 import { CapacitorSQLite, SQLiteConnection, SQLiteDBConnection } from '@capacitor-community/sqlite';
-import { sign } from 'crypto';
 import { Transaction } from '../model/transaction.model';
 import { ReplaySubject } from 'rxjs';
 import { schema } from './schema';
@@ -34,11 +33,6 @@ export class DatabaseService {
     this.initStatus$.next(true);
   }
 
-  async loadTransactions(){
-    const transactions = await this.db.query('SELECT * FROM TXN');
-    this.transactions$.next(transactions.values || null);
-  }
-
   async getTransactionsByMonthYear(monthYear: string): Promise<Transaction[]>{
     const query = ` SELECT * FROM TXN WHERE monthYear = '${monthYear}'`;
     const result = await this.db.query(query);
@@ -46,19 +40,19 @@ export class DatabaseService {
   }
 
   async addTransaction(transaction: Transaction){
-    const query = ` INSERT INTO TXN ( dateStr, accountId, categoryId, toAccountId, amount, note, monthYear)
-                    VALUES ('${transaction.dateStr}',${transaction.accountId}, ${transaction.categoryId}, ${transaction.toAccountId || null}, ${transaction.amount}, ${ transaction.note ? '\'' + transaction.note + '\'' : '\'\''}, '${transaction.monthYear}')
+    const query = ` INSERT INTO TXN ( dateStr, transactionType, accountId, categoryId, toAccountId, amount, note, monthYear)
+                    VALUES ('${transaction.dateStr}','${transaction.transactionType}',${transaction.accountId}, ${transaction.categoryId || null}, ${transaction.toAccountId || null}, ${transaction.amount}, ${ transaction.note ? '\'' + transaction.note + '\'' : '\'\''}, '${transaction.monthYear}')
     `;
     const result = await this.db.execute(query);
-    this.loadTransactions();
     return result;
   }
 
   async updateTransaction(transaction: Transaction){
     const query = ` UPDATE TXN SET 
     dateStr = '${transaction.dateStr}', 
+    transactionType = '${transaction.transactionType}',
     accountId = ${transaction.accountId},
-    categoryId = ${transaction.categoryId},
+    categoryId = ${transaction.categoryId || null},
     toAccountId = ${transaction.toAccountId || null},
     amount = ${transaction.amount}, 
     note = ${ transaction.note ? '\'' + transaction.note + '\'' : '\'\''},
@@ -66,7 +60,6 @@ export class DatabaseService {
     WHERE id = ${transaction.id}`;
 
     const result = await this.db.execute(query);
-    this.loadTransactions();
     return result;
   }
 }
